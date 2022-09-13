c27328052bd5159048f77cbe997e938ba15318fa
Kwame Cody <kwamecody@gmail.com>
2022-09-13 20:26:19 +0000

    Added map and social media icons

--- ais.js
+++ ais.js
@@ -17,3 +17,31 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+/*Testimony slides*/
+
+let testimonyIndex = 0;
+showTesimonySlides();
+
+function showTesimonySlides() {
+  let i;
+  let testimonySlides = document.getElementsByClassName("testimony")
+  let dots = document.getElementsByClassName("dots");
+  for (i = 0; i < testimonySlides.length; i++) {
+    testimonySlides[i].style.display = "none";
+}
+
+testimonyIndex++;
+  if (testimonyIndex > testimonySlides.length) {testimonyIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  testimonySlides[testimonyIndex-1].style.display = "block";  
+  dots[testimonyIndex-1].className += " active";
+  setTimeout(showTesimonySlides, 2000);
+
+
+}
+
+
